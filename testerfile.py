bb0807ac72fbe27e3ee9ae2f049ea3db0f2d1c75
Yash Srivastava <52215319+yashsrivastava12345@users.noreply.github.com>
2024-07-01 14:09:12 +0530

    Add files via upload

--- testerfile.py
+++ testerfile.py
@@ -1,20 +1,25 @@
 from deepface import DeepFace
 import cv2
 # Example usage
-img_path = "196.jpeg"
+img_path = ("0.jpeg")
 db_path = "D:\\New folder\\Face Detection\\DB"
 img = ""
 # Perform face recognition using DeepFace
-dfs = DeepFace.find(img_path=img_path, db_path=db_path)
-print(dfs)
-#print(dfs[[identity]])
-for i in dfs:
-    for j in i:
-        img=dfs[0][j].to_string(index=False)
-        imge = cv2.imread(img)
-        cv2.imshow("My image", imge)
-        cv2.waitKey(0)
+try:
+    dfs = DeepFace.find(img_path=img_path, db_path=db_path)
+    for i in dfs:
+        for j in i:
+            img = dfs[0][j].to_string(index=False)
+            imge = cv2.imread(img)
+            cv2.imshow("My image", imge)
+            cv2.waitKey(0)
+            break
         break
-    break
-print(img)
+    print(img)
+except ValueError:
+    print("Image not found")
+#print(dfs)
+#print(dfs[[identity]])
+
+
 
